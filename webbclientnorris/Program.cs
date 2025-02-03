384d9de78b697c6b97ed3f49876c989a25d6ff15
Hugo Granberg <hugo.granberg@edu.lycksele.se>
2025-02-03 13:20:10 +0100

    first commit

---
+++ webbclientnorris/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Data;
+using System.Net.WebSockets;
+using System.Text;
+
+class Program{
+  static async Task Main(){
+ Uri goblin = new Uri("ws://echo.webbsocket.org");
+ ClientWebSocket goblinsocket = new ClientWebSocket();
+ await goblinsocket.ConnectAsync(goblin, default);
+ string goblinstring = "ez";
+  byte[] goblinarray = Encoding.UTF8.GetBytes(goblinstring);
+ArraySegment<byte>smsg = new(goblinarray);
+await goblinsocket.SendAsync(smsg,WebSocketMessageType.Text,true,CancellationToken.None);
+byte[] ezhomelessgoblinres = new byte[1024];
+var ezgoblinnoobhahahha = await goblinsocket.ReceiveAsync(new ArraySegment<byte>(ezhomelessgoblinres),CancellationToken.None);
+string ahhahagoblimotagennoobez = Encoding.UTF8.GetString(ezhomelessgoblinres,0,ezgoblinnoobhahahha.Count);
+  }
+}
